03ba11582f3e094c8448f35b71396fe868e7c5d1
Jiawen Geng <technicalcute@gmail.com>
2023-09-19 13:02:45 +0000

    feat: add notification

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,15 +1,20 @@
 'use client';
 
 import { Button, Group, useMantineColorScheme } from '@mantine/core';
+import { notifications } from '@mantine/notifications';
 
 export function ColorSchemeToggle() {
   const { setColorScheme } = useMantineColorScheme();
+  const setColor = function (color: string) {
+    setColorScheme(color);
+    notifications.show({ message: color, position: 'top-left' });
+  };
 
   return (
     <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+      <Button onClick={() => setColor('light')}>Light</Button>
+      <Button onClick={() => setColor('dark')}>Dark</Button>
+      <Button onClick={() => setColor('auto')}>Auto</Button>
     </Group>
   );
 }
